f206f0efdfaeb42d7c45e65fdd3ceba05e3b1e21
Stephen Kirk <stephen.kirk@toasttab.com>
2021-08-20 11:04:23 +0100

    Fix broken test

--- src/test/kotlin/com/toasttab/pulseman/test/SampleTestClassTest.kt
+++ src/test/kotlin/com/toasttab/pulseman/test/SampleTestClassTest.kt
@@ -6,6 +6,6 @@ import org.junit.jupiter.api.Test
 class SampleTestClassTest {
     @Test
     fun `Sample test 1`() {
-        assertThat(SampleTestClass().testFunction()).isFalse
+        assertThat(SampleTestClass().testFunction()).isTrue
     }
 }
